1654fe19aa37397775efd6728468fc17c8ebaa60
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2021-03-20 17:07:12 +0900

    build.pyを更新

--- tools/build.py
+++ tools/build.py
@@ -14,6 +14,8 @@ import diff_archiver
 import hashlib
 import json
 import sys
+import urllib.request
+import zipfile
 sys.path.append(os.getcwd())
 import constants
 
@@ -69,7 +71,7 @@ for item in constants.PACKAGE_CONTAIN_ITEMS:
 	if os.path.isdir(item):
 		shutil.copytree(item, os.path.join(package_path, item))
 	if os.path.isfile(item):
-		shutil.copyfile(item, os.path.join(package_path, os.path.basename(item)))
+		shutil.copyfile(item, os.path.join(package_name, os.path.basename(item)))
 for elem in glob.glob("public\\*"):
 	if os.path.isfile(elem):
 		shutil.copyfile(elem, os.path.join(package_path, os.path.basename(elem)))
@@ -84,24 +86,34 @@ shutil.make_archive("%s-%s" % (constants.APP_NAME, build_filename),'zip','dist')
 if build_filename=="snapshot":
 	print("Skipping batch archiving because this is a snapshot release.")
 else:
+	archive_name = "%s-%s.zip" % (constants.APP_NAME, build_filename)
 	if constants.BASE_PACKAGE_URL is not None:
 		print("Making patch...")
-		archive_name = "%s-%s.zip" % (constants.APP_NAME, build_filename)
 		patch_name = "%s-%spatch" % (constants.APP_NAME, build_filename)
 		archiver=diff_archiver.DiffArchiver(constants.BASE_PACKAGE_URL, archive_name, patch_name,clean_base_package=True, skip_root = True)
 		archiver.work()
-		print("computing hash...")
-		with open(archive_name, mode = "rb") as f:
-			content = f.read()
-			package_hash = hashlib.sha1(content).hexdigest()
+	if constants.UPDATER_URL is not None:
+		print("downloading updater...")
+		urllib.request.urlretrieve(constants.UPDATER_URL, "updater.zip")
+		print("writing updater...")
+		with zipfile.ZipFile("updater.zip", "r") as zip:
+			zip.extractall()
+		with zipfile.ZipFile(archive_name, mode = "a") as zip:
+			zip.write("ionic.zip.dll", "%s/ionic.zip.dll" % (constants.APP_NAME))
+			zip.write("updater.exe", "%s/updater.exe" % (constants.APP_NAME))
+	print("computing hash...")
+	with open(archive_name, mode = "rb") as f:
+		content = f.read()
+		package_hash = hashlib.sha1(content).hexdigest()
+	if constants.BASE_PACKAGE_URL is not None:
 		with open(patch_name+".zip", mode = "rb") as f:
 			content = f.read()
 			patch_hash = hashlib.sha1(content).hexdigest()
-		print("creating package info...")
-		with open("%s-%s_info.json" % (constants.APP_NAME, build_filename), mode = "w") as f:
-			info = {
-				"package_hash": package_hash,
-				"patch_hash": patch_hash
-			}
-			json.dump(info, f)
+	print("creating package info...")
+	with open("%s-%s_info.json" % (constants.APP_NAME, build_filename), mode = "w") as f:
+		info = {}
+		info["package_hash"] = package_hash
+		if constants.BASE_PACKAGE_URL is not None:
+			info["patch_hash"] = patch_hash
+		json.dump(info, f)
 print("Build finished!")
