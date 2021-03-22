8855d7f72319abef74fc1fe9dda0d896d68a7cd2
Kazto Kitabatake <kashiwamochi@mbr.nifty.com>
2021-03-22 09:18:40 +0900

    ログ出力を修正

--- views/main.py
+++ views/main.py
@@ -76,7 +76,7 @@ class MainView(BaseView):
 			timestamp,
 		]
 		self.logList.Append(data)
-		self.log.info("display: " + ",".join(data) + "\n")
+		self.log.info("display: " + ",".join(data))
 
 	def applyHotKey(self):
 		self.hotkey = hotkeyHandler.HotkeyHandler(None,hotkeyHandler.HotkeyFilter().SetDefault())
