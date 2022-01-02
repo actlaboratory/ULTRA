b3be24d782bd2ef3859ce58887f97e3d4a33e330
Kazto Kitabatake <kitabatake1013@outlook.jp>
2022-01-02 20:38:26 +0900

    Fix message

--- menuItemsDic.py
+++ menuItemsDic.py
@@ -21,7 +21,7 @@ dic={
 	"TC_UPDATE_USER": _("ユーザ情報を更新(&U)"),
 	"TC_ADD_TW": _("Twitterでフォローしているユーザを一括追加(&F)"),
 	"TC_RECORD_ARCHIVE": _("過去ライブのダウンロード(&A)") + "...",
-	"TC_RECORD_ALL": _("過去ライブの一括録画(&P)"),
+	"TC_RECORD_ALL": _("過去ライブの一括ダウンロード(&D)"),
 	"TC_RECORD_USER": _("指定したユーザのライブを録画(&R)") + "...",
 	"TC_REMOVE_TOKEN": _("アクセストークンを削除"),
 	"TC_SET_TOKEN": _("アクセストークンを設定(&T)"),
