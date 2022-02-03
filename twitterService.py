7b5884c005996c25a1702e721dec946e02a56534
Kazto Kitabatake <kitabatake1013@outlook.jp>
2022-02-03 14:21:41 +0900

    Revert "Revert "Fix twitter service""
    This reverts commit 1949de805654eca6d049102d7a300abb93d56704.

--- twitterService.py
+++ twitterService.py
@@ -62,14 +62,14 @@ def getFollowList(token,target):
 	try:
 		user = twitterApi.get_user(screen_name=target)
 		friendsCount = user.friends_count
-		friends = tweepy.Cursor(twitterApi.friends,screen_name=target,include_user_entities=False,skip_status=True,count=200).items()
+		friends = tweepy.Cursor(twitterApi.get_friends,screen_name=target,include_user_entities=False,skip_status=True,count=200).items()
 		for friend in friends:
 			ret.append(friend.screen_name)
 		return ret
-	except tweepy.error.RateLimitError:
+	except tweepy.TooManyRequests:
 		log.error("rateLimitError")
 		return ret
-	except tweepy.error.TweepError as e:
+	except tweepy.TweepyException as e:
 		log.error(e)
 		log.error("%s" %(e.response))
 		simpleDialog.errorDialog(_("Twitterからフォローリストを取得できませんでした。指定したユーザが存在しないか、フォローしていない非公開アカウントである可能性があります。"))
