4b69bf7a5d38bd9dcda3c2657dd4e4b6b60d2c6e
Kazto Kitabatake <kitabatake1013@outlook.jp>
2022-02-09 09:15:13 +0900

    hide unnecessary columns in tokenManager

--- views/spacesTokenManager.py
+++ views/spacesTokenManager.py
@@ -29,13 +29,14 @@ class Dialog(BaseDialog):
 
 		# 情報の表示
 		self.creator = views.ViewCreator.ViewCreator(self.viewMode, self.panel, self.sizer, wx.VERTICAL, 20, style=wx.EXPAND | wx.ALL)
-		self.hListCtrl, self.hListStatic = self.creator.listCtrl(_("アカウント"), None, wx.LC_REPORT | wx.BORDER_RAISED, (800, 300), wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
+		self.hListCtrl, self.hListStatic = self.creator.virtualListCtrl(_("アカウント"), None, wx.LC_REPORT | wx.BORDER_RAISED, (800, 300), wx.ALL | wx.ALIGN_CENTER_HORIZONTAL)
 		self.hListCtrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.ItemSelected)
 		self.hListCtrl.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.ItemSelected)
 		self.hListCtrl.AppendColumn(_("ユーザ名"), format=wx.LIST_FORMAT_LEFT, width=250)
 		self.hListCtrl.AppendColumn(_("名前"),format=wx.LIST_FORMAT_LEFT,width=250)
 		self.hListCtrl.AppendColumn(_("規定のアカウント"), format=wx.LIST_FORMAT_LEFT, width=110)
 		self.hListCtrl.AppendColumn(_("ID"), format=wx.LIST_FORMAT_LEFT, width=110)
+		self.hListCtrl.SetColumnsOrder([0, 1, 2])
 
 		# 処理ボタン
 		self.creator = views.ViewCreator.ViewCreator(self.viewMode, self.panel, self.creator.GetSizer(), wx.HORIZONTAL, 20, "", wx.ALIGN_RIGHT)
