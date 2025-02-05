3ffc93c0d4f178790070780467ee2b315f34cf7c
Kazto Kitabatake <kitabatake1013@outlook.jp>
2025-02-06 08:27:32 +0900

    ユーザーリストの読み込みエラーのログ記録を修正

--- sources/twitcasting.py
+++ sources/twitcasting.py
@@ -376,7 +376,7 @@ class Twitcasting(SourceBase):
 			with open(constants.TC_USER_DATA, "r", encoding="utf-8") as f:
 				try:
 					self.users = json.load(f)
-				except json.JSONDecodeError as e:
+				except json.decoder.JSONDecodeError as e:
 					# ログにファイルの内容を書き込むため、戦闘に戻す
 					f.seek(0)
 					self.log.error("Json decode error: %s" % f.read())
